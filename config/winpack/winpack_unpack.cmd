f22bb9583c80bff24872de85df25501a4d1c0c96
Alexander Alekhin <alexander.alekhin@intel.com>
2020-02-06 17:49:14 +0300

    winpack: extra tries to unpack archive

--- config/winpack/winpack_unpack.cmd
+++ config/winpack/winpack_unpack.cmd
@@ -1,7 +1,25 @@
 @echo off
 md distrib
 pushd distrib || goto :error
-7z x ..\distrib.7z.exe || (sleep 20 && 7z x ..\distrib.7z.exe || echo.Extract failed >&2 && exit /B 1 )
+7z x ..\distrib.7z.exe
+if "%ERRORLEVEL%"=="0" goto :unpacked
+sleep 20
+7z x ..\distrib.7z.exe
+if "%ERRORLEVEL%"=="0" goto :unpacked
+sleep 60
+7z x ..\distrib.7z.exe
+if "%ERRORLEVEL%"=="0" goto :unpacked
+sleep 120
+7z x ..\distrib.7z.exe
+if "%ERRORLEVEL%"=="0" goto :unpacked
+sleep 300
+7z x ..\distrib.7z.exe
+if "%ERRORLEVEL%"=="0" goto :unpacked
+
+echo.Extract failed >&2
+exit /B 1
+
+:unpacked
 popd || goto :error
 
 exit /B 0
